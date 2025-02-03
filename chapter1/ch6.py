4d497f1541c520d89c4fda810b743cad40ba8dd4
Suraj Mishra <mishrasur7@gmail.com>
2025-02-03 22:08:47 +0200

    ch 1.6 added

---
+++ chapter1/ch6.py
@@ -0,0 +1,15 @@
+def num_collections_sum():
+    sum = 0
+    while True:
+        user_input = input()
+        try:
+            number = float(user_input)
+            if number == 0:
+                print(f'The grand total is {sum}.')
+                break
+            sum += number
+            print(f'The total is {sum}.')
+        except ValueError:
+            print("That wasn't a number.")
+
+num_collections_sum()
